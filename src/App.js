8de4d041221b244935041cb7d4936b621ae7bc0f
alpagu872 <83309872+alpagu872@users.noreply.github.com>
2022-02-11 19:53:01 +0300

    useEffect and useState using

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 
-import React from 'react';
+import React, {useEffect,useState}from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  SafeAreaView, StyleSheet, Text, View, Button, 
 } from 'react-native';
+
+
+function App() {
+
+  const [number, setNumber] = React.useState(0)
+  const [counter, setCounter] = useState(100)
+
+  useEffect(() => {
+    console.log("Number updated");
+  }, [number]);
+
+  useEffect(() =>{
+    console.log("Counter Updated");
+  }, [counter]);
+
+  return (
+    <SafeAreaView>
+
+      <Text>Hello LifeCycle</Text>
+
+      <Text>Number: {number}</Text>
+      <Text>Counter: {counter}</Text>
+      <Button title="Up!" onPress={() => setNumber(number + 1)} style={{margin:10}}/>
+      
+      <Button title="Up Counter! " onPress={() => setCounter(counter+100)} style={{padding:10}}/>
+
+    </SafeAreaView>
+  )
+}
 
 
 
